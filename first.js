b1e14bce7c114975777ca992a923e5dcacb55e9f
sammar <bhattisammar02@gmail.com>
2025-07-26 11:47:50 +0500

    objects

--- first.js
+++ first.js
@@ -1,1 +1,34 @@
-console.log("fatima");
+/*fullname = "sammar abbas"
+console.log(fullname)
+age = 24
+console.log(age)
+/*
+null means khali hein
+undefined means kch define nahi */
+/*isfollow = true
+console.log(isfollow)
+a = null
+console.log(a)
+{
+    let a = 5;   
+    console.log(a)
+}
+{
+    let a = 19;
+    console.log(a)
+}*/
+const student = {
+    fullname :"sammar",
+    age: 32,
+    cgoa:3.2,
+    ispass:true,
+
+};
+/** variable ki value access karna ka liya obj.key and 2nd method is obj["key"] */
+
+/** agar value inc karwani hein obj mein to ya method kary ga */
+student.age = student.age + 1;
+console.log(student.age);
+/**............................................. */
+student.fullname = "sammar abbas"
+console.log(student.fullname);
