f242d3cd49d20f5204ce9fe6bbf31e713089b85d
sammar <bhattisammar02@gmail.com>
2025-07-27 19:18:36 +0500

    new objects

--- first.js
+++ first.js
@@ -17,18 +17,83 @@ console.log(a)
     let a = 19;
     console.log(a)
 }*/
-const student = {
-    fullname :"sammar",
-    age: 32,
-    cgoa:3.2,
-    ispass:true,
+//const student = {
+  //  fullname :"sammar",
+    //age: 32,
+    //cgoa:3.2,
+    //ispass:true,
 
-};
-/** variable ki value access karna ka liya obj.key and 2nd method is obj["key"] */
+//};
+/** variable ki value access karna ka liya obj.key and 2nd method is obj["key"]. const mein obj ki key ko update kar sakta hein  const obj ->key*/
 
 /** agar value inc karwani hein obj mein to ya method kary ga */
-student.age = student.age + 1;
-console.log(student.age);
+//student.age = student.age + 1;
+//console.log(student.age);
 /**............................................. */
-student.fullname = "sammar abbas"
-console.log(student.fullname);
+//student.fullname = "sammar abbas"
+//console.log(student.fullname);
+/**example */
+/*const profile = {
+    username : "sammar",
+    isfollow : false,
+    followers : 123,
+    following : 1234,
+}; 
+console.log(typeof profile.isfollow) */
+// 2nd part of the chapter
+//console.log("hello sammar")
+//artimetic operator
+//let a = 2;
+//let b = 3;
+//let c = a+b;
+//let d = a*b;
+//console.log("a + b =", c);
+//console.log("a*b",d);
+//Logical operator
+//let a = 6;
+//let b = 5;
+//let cond1 = a > b;
+//let cond2 = b == 6;
+//console.log("con1 || cond2 =", cond1 || cond2);
+//condition 
+/*let age = 16;
+if(age >= 18) {
+    console.log("vote");
+}else{
+    console.log("not vote");
+}*/
+//alert("hello beta");
+//let name = prompt("hello!");
+//console.log(name);  
+/*let num = prompt("helllo");
+if(num %5 == 0 ){
+    console.log(num,"is multiple of 5");
+}else{
+    console.log(num,"is not multiple of 5");    
+}*/
+//for loop
+// for(let i =1; i <=5; i++){
+//     console.log("sammar");
+// }
+// for of loop used for string and array
+// let str = "sammar"
+// let size = 0;
+// for(let i of str){
+//     console.log("i=",i);
+//     size++;
+// }
+// console.log("size of",size);
+
+//for-in loop
+// let spestring = "sammar abbas ";
+// console.log(spestring);
+
+
+let obj = {
+    item: "pen",
+    price: 10,
+};
+
+let output = `The cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output); 
+
